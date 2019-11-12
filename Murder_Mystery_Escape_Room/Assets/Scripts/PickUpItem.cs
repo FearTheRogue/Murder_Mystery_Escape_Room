1ca0f4d4361ae6b656fe3dea7c06aa292982e658
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-11-12 16:10:10 +0000

    Sprint 3 - Picking Up An Object
    Major Updates:
    - Player now has the ability to pick up an object using Raycasts
    - Player can now put down an object
    - Objects name and description can be seen on the UI when object is picked up
    
    Minor Updates:
    - Added a crosshair in the center of the screen, so its easier to tell when the player can pick up an object

---
+++ Murder_Mystery_Escape_Room/Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUpItem : MonoBehaviour
+{
+    public Transform theDest;
+    public Text nameText, DescText;
+
+    public string nameOfObject, DescOfObject;
+
+    float maxDistance = 10f;
+
+    void OnMouseDown()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.localPosition, out hit, maxDistance))
+        {
+            Debug.Log("Object: " + this.name);
+
+            //GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            GetComponent<Rigidbody>().freezeRotation = true;
+
+            this.transform.position = theDest.position;
+            this.transform.parent = GameObject.Find("Object Destination").transform;
+
+            nameText.text = "Name: " + nameOfObject;
+            DescText.text = "Description: " + DescOfObject;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        this.transform.parent = null;
+
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().freezeRotation = false;
+        //GetComponent<BoxCollider>().enabled = true;
+
+        nameText.text = "";
+        DescText.text = "";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Nope");
+    }
+
+    //void FixedUpdate()
+    //{
+    //    RaycastHit hit; 
+
+    //    if(Physics.Raycast(transform.position, transform.forward, out hit, maxDistance) && hit.collider.gameObject.CompareTag("Player"))
+    //    {
+    //        Debug.Log("Object: " + hit.point);
+    //    }
+    //}
+}

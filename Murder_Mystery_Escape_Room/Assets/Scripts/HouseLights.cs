14d5160318745998d089e55e39430cb9afe3c302
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2019-12-12 22:10:42 +0000

    Sprint Update 5 & 6
    Major Updates:
    + Added Model House to replace basic model house
    + Added Game Manager Script
    
    Minor Updates:
    + Added Tree to Main Menu screen
    + Added Fog to Main Menu screen

---
+++ Murder_Mystery_Escape_Room/Assets/Scripts/HouseLights.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseLights : MonoBehaviour
+{
+    public Material lightsOn;
+
+    void Start()
+    {
+        lightsOn = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            lightsOn.SetColor("_EmissionColor", Color.black);
+        } 
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            lightsOn.SetColor("_EmissionColor", Color.yellow);
+        }
+
+    }
+}

f779302357c6bbdac13a5660054165005a942671
FearTheRogue <43548013+FearTheRogue@users.noreply.github.com>
2020-02-11 17:31:19 +0000

    Starting Beta Version
    Major Updates:
    + Starting implementing room blender file
    + Unity project now using HDRP

--- Murder_Mystery_Escape_Room/Assets/Scripts/PlayerMovement.cs
+++ Murder_Mystery_Escape_Room/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
 
     //public IsPlayerMoving playerMoving;
 
@@ -15,6 +16,12 @@ public class PlayerMovement : MonoBehaviour
     public ObjectPickUp objectPickUp;
 
     public Vector3 move;
+    public Vector3 velocity;
+    public bool isGrounded;
+
+    public Transform groundCheck;
+    public float grounndDistance = 0.4f;
+    public LayerMask groundMask;
 
     public Vector3 isNotMoving;
 
@@ -31,12 +38,23 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!objectPickUp.GetComponent<ObjectPickUp>().isObjectPickUp)
         {
+            isGrounded = Physics.CheckSphere(groundCheck.position, grounndDistance, groundMask);
+
+            if(isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
             float x = Input.GetAxis("Horizontal");
             float z = Input.GetAxis("Vertical");
 
             move = transform.right * x + transform.forward * z;
 
             controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+
+            controller.Move(velocity * Time.deltaTime);
 
             if (move == isNotMoving)
             {
